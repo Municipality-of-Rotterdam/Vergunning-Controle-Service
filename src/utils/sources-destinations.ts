fd89c332e29eecb0af5629383613a250f9e72d3f
Kathrin Dentler <kathrin.dentler@triply.cc>
2024-03-05 14:00:38 +0100

    Update TriplyETL and integrate DTAP

--- src/utils/sources-destinations.ts
+++ src/utils/sources-destinations.ts
@@ -1,5 +1,5 @@
 import { Source } from '@triplyetl/etl/generic'
-import { get_account, get_dataset } from './dtap.js'
+import { getAccount, getDataset } from './dtap.js'
 
 export const source = {
   people: Source.file('static/people.csv'),
@@ -8,6 +8,6 @@ export const source = {
 
 export const destination = {
   vergunningscontroleservice: {
-    account: get_account(), dataset: { name: get_dataset('vergunningscontroleservice'), displayName: 'Vergunningscontroleservice', description: 'Gepubliceerd door TriplyETL' }
+    account: getAccount(), dataset: { name: getDataset('vergunningscontroleservice'), displayName: 'Vergunningscontroleservice', description: 'Gepubliceerd door TriplyETL' }
   }
 }
