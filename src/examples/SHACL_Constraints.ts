b9530ce159691e25847dbce98dee51d6d37cbc56
Philippe Renzen <philippe.renzen@triply.cc>
2024-03-08 10:22:17 +0100

    Updates examples and version of TriplyETL

--- src/examples/SHACL_Constraints.ts
+++ src/examples/SHACL_Constraints.ts
@@ -1,48 +1,92 @@
-// this ETL only uses SHACL Constraints for the validation report, and creates data using TriplyETL Ratt
-import { logQuads } from '@triplyetl/etl/debug'
-import { declarePrefix, Etl, fromJson, ifElse, Source, when, whenForEach } from '@triplyetl/etl/generic'
-import { addIri, literal, str, triple } from '@triplyetl/etl/ratt'
+import { declarePrefix, Destination, Etl, fromJson, Source, toRdf, when, whenForEach } from '@triplyetl/etl/generic'
+import { addHashedIri, addIri, literal, pairs, triple } from '@triplyetl/etl/ratt'
+import { a, dbo, rdfs, xsd } from '@triplyetl/etl/vocab'
 import { validate } from '@triplyetl/etl/shacl'
-import { a, xsd } from '@triplyetl/etl/vocab'
 
-// Declare the base for all Iri's:
-const baseIri = declarePrefix('https://example.org/')
+// Declare the base for all Iri's & other Iri's used in this ETL:
+const baseIri = declarePrefix('https://example.org')
+const def = baseIri.concat('def')
+const id = baseIri.concat('id')
+const woonwijk = def.concat('/Woonwijk')
+const hoogte = def.concat('/Hoogte')
+const gebouwType = def.concat('/GebouwType')
+const aantalPersonen = def.concat('/AantalPersonen')
+const heeftBouwlaag = def.concat('/heeftBouwlaag')
+const verdieping = def.concat('/Verdieping')
 
 export default async function (): Promise<Etl> {
   // Create an extract-transform-load (ETL) process.
   const etl = new Etl({ baseIri })
   etl.use(
     fromJson(Source.file('./src/examples/static/example_mock_data_building.json')),
-    whenForEach('@buildings', [
+    whenForEach('@gebouwen', [
       addIri({
-        prefix: baseIri.concat('Building'),
+        prefix: id.concat('/'),
         content: '@id',
-        key: '_buildingID'
+        key: '_gebouwID'
       }),
-      triple('_buildingID', a, baseIri.concat('Building')),
-      when('height',
-        triple('_buildingID', baseIri.concat('Height'), literal('height', xsd.integer)),
-        // Check if there is a height violation:
-        ifElse(
-          {
-            if: ctx => ctx.getNumber('height') > 15,
-            then: triple('_buildingID', baseIri.concat('HeightViolation'), literal(str('true'), xsd.boolean))
-          },
-          {
-            else: triple('_buildingID', baseIri.concat('HeightViolation'), literal(str('false'), xsd.boolean))
-          }
+      triple('_gebouwID', a, dbo.Building),
+      when('hoogte',
+        triple('_gebouwID', hoogte, literal('hoogte', xsd.integer))
+      ),
+      when('gebouwType',
+        triple('_gebouwID', gebouwType, literal('gebouwType', xsd.string))
+      ),
+      when('woonwijkCode',
+        triple('_gebouwID', woonwijk, literal('woonwijkCode', xsd.string))
+      ),
+      when('aantalPersonen',
+        triple('_gebouwID', aantalPersonen, literal('aantalPersonen', xsd.integer))
+      ),
+      addHashedIri({
+        content: '@id',
+        prefix: baseIri.concat('Bouwlaag/'),
+        key: '_bouwlaag'
+      }),
+      triple('_gebouwID', heeftBouwlaag, '_bouwlaag'),
+      whenForEach('bouwlaag',
+        addHashedIri({
+          content: ['$parent.@id', 'id'],
+          prefix: baseIri.concat('bouwlaagID/'),
+          key: '_bouwlaagID'
+        }),
+        pairs('$parent._bouwlaag',
+          [
+            a, baseIri.concat('Bouwlaag')
+          ],
+          [
+            def.concat('/bouwlaagElementen'), '_bouwlaagID'
+          ]
+        ),
+        when('id',
+          triple('_bouwlaagID', rdfs.label, 'id')
+        ),
+        when('verdieping',
+          triple('_bouwlaagID', verdieping, 'verdieping')
+        ),
+        when('hoogte',
+          triple('_bouwlaagID', hoogte, 'hoogte')
         )
       ),
-      when('buildingType',
-        triple('_buildingID', baseIri.concat('BuildingType'), literal('buildingType', xsd.string))
-      ),
-      when('numberOfPeople',
-        triple('_buildingID', baseIri.concat('NumberOfPeople'), literal('numberOfPeople', xsd.integer))
-      ),
-      logQuads()
+      when('opmerking',
+        triple('_gebouwID', rdfs.comment, 'opmerking')
+      )
     ]),
-    logQuads(),
-    validate(Source.file('./src/examples/static/building_knowledge_model.ttl'))
+    validate(Source.file('./src/examples/static/building_knowledge_model.ttl')),
+
+    // Dataset lokaal naar bestand schrijven
+    toRdf(Destination.file('./src/examples/MWE_Constraint.ttl'))
+
+    // Dataset uploaden naar demo.triplydb.com
+    // toTriplyDb({
+    //   dataset: 'example-SHACL-Constraint-data',
+    //   opts: {
+    //     triplyDb: {
+    //       token: process.env.TRIPLYDB_TOKEN,
+    //       url: 'https://api.demo.triplydb.com'
+    //     }
+    //   }
+    // })
   )
   return etl
 }
