e31efc150d58cc2cdb7cd3c511b37fa3da6cfbd6
Kathrin Dentler <kathrin.dentler@triply.cc>
2024-05-31 15:41:08 +0200

    Skip IDS step during development

--- src/main.ts
+++ src/main.ts
@@ -4,16 +4,19 @@ import { baseIri } from "./utils/declarations.js";
 import { ruleIdShaclConstraintDictionary, vcsEtl, vcsGenerateShacl } from "./VCS/VcsEtl.js";
 
 const ifcFile = "static/Kievitsweg_R23_MVP_IFC4.ifc";
-const idsFile = "static/example_data/IDS Rotterdam BIM.ids";
 
 export default async function (): Promise<Etl> {
   // Create an extract-transform-load (ETL) process.
   const etl = new Etl({ baseIri, defaultGraph: baseIri.concat("default") });
 
   etl.use(
-    await vcsEtl(ifcFile, idsFile, {
-      baseIRI: "https://www.rotterdam.nl/vcs/",
-    }),
+    // without ids check
+    await vcsEtl(ifcFile, { baseIRI: "https://www.rotterdam.nl/vcs/" }),
+
+    // with ids check
+    // const idsFile = "static/example_data/IDS Rotterdam BIM.ids";
+    // await vcsEtl(ifcFile, idsFile, { baseIRI: "https://www.rotterdam.nl/vcs/" }),
+
     vcsGenerateShacl(ruleIdShaclConstraintDictionary),
     toTriplyDb(destination.vergunningscontroleservice),
   );
