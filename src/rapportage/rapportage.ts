e508a7245bb4606af76201f8582a34c0a4d3fa67
Niels Steenbergen <niels.steenbergen@triply.cc>
2024-06-25 12:50:09 +0200

    Use base64url for model

--- src/rapportage/rapportage.ts
+++ src/rapportage/rapportage.ts
@@ -25,13 +25,23 @@ export const rapportage = async ({
   if (!dataset) throw new Error(`Kon de dataset ${datasetName} niet vinden in TriplyDB`)
 
   const gltfAsset = await dataset.getAsset('gebouw.gltf')
+  const model = await fetch(gltfAsset.getInfo().url, {
+    headers: {
+      'content-type': 'application/json',
+      Accepts: 'application/sparql-results+json, application/n-triples',
+      Authorization: 'Bearer ' + process.env.TRIPLYDB_TOKEN!,
+    },
+  })
+  const blob = await model.blob()
+  const buffer = Buffer.from(await blob.arrayBuffer())
+  const urlBase64Encoded = buffer.toString('base64url')
 
   log('Genereren van het validatie rapport', 'Validatie rapport')
 
   const props = {
     gebouw: pointer.out(rpt('building')).value.toString(),
     geoData: geoData,
-    gltfUrl: gltfAsset.getInfo().url,
+    gltfUrl: `data:${urlBase64Encoded}`,
     polygon: {
       type: 'Feature',
       geometry: {
@@ -49,7 +59,7 @@ export const rapportage = async ({
   }
 
   const html = renderToStaticMarkup(RapportageTemplate(props))
-  writeFile(`${outputsDir}/validatie-rapport.html`, html)
+  await writeFile(`${outputsDir}/validatie-rapport.html`, html)
   const fileId = `validatie-rapport.html`
 
   try {
