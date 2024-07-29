84c6db8690c5319d315f207ced2e119816cba78a
Niels Steenbergen <niels.steenbergen@triply.cc>
2024-07-29 13:29:40 +0200

    Fix type error

--- src/controles/01-ruimtelijke-plannen/wonen/common.ts
+++ src/controles/01-ruimtelijke-plannen/wonen/common.ts
@@ -1,7 +1,8 @@
 import { Controle } from '@root/core/Controle.js'
 import { Data } from '../common.js'
+import { StepContext } from '@root/core/executeSteps.js'
 
-export default class _ extends Controle<Data, {}> {
+export default class _ extends Controle<StepContext & Data, {}> {
   public name = 'Wonen'
   async run() {
     return {}
