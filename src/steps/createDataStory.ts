3b95d10fb3234c24e1e98f6858dbcaf2521362f6
Daniel Beeke <daniel.beeke@triply.cc>
2024-08-14 16:12:44 +0200

    copy queries to the current user

--- src/steps/createDataStory.ts
+++ src/steps/createDataStory.ts
@@ -7,6 +7,7 @@
  * - We need to generate the data story for these copied queries.
  */
 
+import crypto from 'crypto';
 import { parseHTML } from 'linkedom';
 
 import { getAccount } from '@root/helpers/getAccount.js';
@@ -18,22 +19,12 @@ export default {
   name: 'Maak data verhaal',
   description: 'Maak het data verhaal aan de hand van de queries in de rule repository',
   async run(context: Context) {
-    await ensureQueriesForDataset(context)
     const triply = App.get({ token: process.env.TRIPLYDB_TOKEN! })
     const account = await triply.getAccount(getAccount())
     const ruleRepository = App.get({ token: process.env.TRIPLYDB_RULE_REPOSITORY_TOKEN! })
     const organization = await ruleRepository.getOrganization('Rotterdam-Rule-Repository')
     const template = await organization.getStory('template')
     const templateContent = (await template.getInfo()).content
-    let [header, ...rest] = templateContent.map((item) => {
-      /** @ts-ignore */
-      delete item.id
-
-      /** @ts-ignore */
-      if (item.query) item.query = item.query.id
-
-      return item
-    })
 
     // Fetches all data-TOKENs
     // const dataAttributes = header.paragraph!.split(/ |\>|\n/g).filter((attribute) => attribute.startsWith('data-'))
@@ -52,17 +43,41 @@ export default {
       'data-ids-rapport-bcf': 'lorem',
     }
 
-    const { document } = parseHTML(header.paragraph!)
-    for (const [key, value] of Object.entries(tokens)) {
-      const elementsWithDataAttribute = [...document.querySelectorAll(`[${key}]`)]
-      for (const element of elementsWithDataAttribute) {
-        element.innerHTML = value
-        if (element.nodeName === 'A') element.setAttribute('href', value)
+    for (const item of templateContent) {
+      /** @ts-ignore */
+      delete item.id
+
+      if (item.type === 'query') {
+        const name = crypto.createHash('md5').update(`${context.datasetName}:${item.query?.name}`).digest('hex')
+
+        let existingQuery
+        try {
+          existingQuery = await account.getQuery(name)
+        } catch {}
+        if (existingQuery) await existingQuery.delete()
+        const addedQuery = await account.addQuery(name, {
+          queryString: item.query!.requestConfig!.payload.query,
+          dataset: context.buildingDataset,
+          serviceType: 'speedy',
+        })
+
+        /** @ts-ignore */
+        item.query = (await addedQuery.getInfo()).id
+      }
+
+      if (item.type === 'paragraph') {
+        const { document } = parseHTML(item.paragraph!)
+        for (const [key, value] of Object.entries(tokens)) {
+          const elementsWithDataAttribute = [...document.querySelectorAll(`[${key}]`)]
+          for (const element of elementsWithDataAttribute) {
+            element.innerHTML = value
+            if (element.nodeName === 'A') element.setAttribute('href', value)
+          }
+        }
+
+        item.paragraph = document.toString()
       }
     }
-
-    const changedHeader = document.toString()
-    header.paragraph = changedHeader
 
     let existingStory
     try {
@@ -70,44 +85,8 @@ export default {
       await existingStory.delete()
     } catch {}
 
-    const story = { content: [header, ...rest] as any[] }
+    const story = { content: templateContent as any[] }
 
     const savedStory = await account.addStory(context.datasetName, story)
-    console.log(savedStory)
   },
 } satisfies Step
-
-/**
- * Copies over the queries from the rule repository for the usage of a data story and a building dataset.
- */
-const ensureQueriesForDataset = async (context: Context) => {
-  const ruleRepository = App.get({ token: process.env.TRIPLYDB_RULE_REPOSITORY_TOKEN! })
-  const organization = await ruleRepository.getOrganization('Rotterdam-Rule-Repository')
-  const queries = organization.getQueries()
-
-  const triply = App.get({ token: process.env.TRIPLYDB_TOKEN! })
-  const account = await triply.getAccount(getAccount())
-
-  for await (const query of queries) {
-    const info = await query.getInfo()
-    const queryText = info.requestConfig?.payload.query!
-
-    // TODO this is problematic, discuss with team, noted in the '_project overview' document.
-    const queryName = context.datasetName.substring(0, 30) + '-' + info.name.substring(0, 9).replaceAll('-', '')
-
-    let existingQuery
-    try {
-      existingQuery = await account.getQuery(queryName)
-    } catch {}
-
-    if (context.cache && existingQuery) continue
-
-    if (existingQuery) await existingQuery.delete()
-    console.log(account)
-    await account.addQuery(queryName, {
-      queryString: queryText,
-      dataset: context.buildingDataset,
-      serviceType: 'speedy',
-    })
-  }
-}
