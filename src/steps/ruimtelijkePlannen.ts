b8e87581c110470aee15800322059c159f279cc1
Niels Steenbergen <niels.steenbergen@triply.cc>
2024-08-16 12:07:35 +0200

    Skip parsing step.

--- src/steps/ruimtelijkePlannen.ts
+++ src/steps/ruimtelijkePlannen.ts
@@ -5,9 +5,10 @@ import { graphExists } from '@root/helpers/existence.js'
 import { SKIP_STEP } from '@root/helpers/skipStep.js'
 import { Context, Step } from '@root/types.js'
 import { sparqlRequest } from '@root/helpers/sparqlRequest.js'
-import { ifc, geo, prov, rdf } from '@root/helpers/namespaces.js'
+import { ifc, geo, sf, prov, rdf } from '@root/helpers/namespaces.js'
 import { geojsonToWKT, wktToGeoJSON } from '@terraformer/wkt'
 import { API } from '@root/helpers/api.js'
+import { Quad } from '@rdfjs/types'
 
 /**
  * Ruimtelijke Plannen Opvragen API
@@ -62,10 +63,8 @@ export default {
         ruimtelijkPlan: plannen,
       }
 
-      // TODO: Is there a more efficient way to get JSON-LD into the store?
-      const nquads = (await jsonld.toRDF(doc, { format: 'application/n-quads' })) as unknown as string
-      const parser = new n3.Parser({ format: 'application/n-quads' })
-      store.addQuads(parser.parse(nquads))
+      const quads = (await jsonld.toRDF(doc)) as Quad[]
+      store.addQuads(quads)
     }
     await context.buildingDataset.importFromStore(store, {
       defaultGraphName: graphName,
