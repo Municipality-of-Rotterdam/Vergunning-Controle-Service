c1fd17f6fcf1b072ce25a3cab1f9446a2e7b1493
Niels Steenbergen <niels.steenbergen@triply.cc>
2024-08-22 10:56:46 +0200

    Do pre-selection for plans.
    Too many API calls later otherwise.

--- src/steps/ruimtelijkePlannen.ts
+++ src/steps/ruimtelijkePlannen.ts
@@ -47,7 +47,29 @@ export default {
       })
       // quads.push(...(await jsonldToQuads({[building.root]: }))
 
-      for (const plan of response['_embedded']['plannen']) {
+      // Select the most recent parapluplan plus the most recent bestemmingsplan
+      // that has a 'dossierstatus' of 'geldend', in accordance with
+      // <https://git.triply.cc/customers/gemeenterotterdam/vergunningscontroleservice/-/issues/32>,
+      const relevantPlans: any[] = response['_embedded']['plannen']
+        .filter((plan: any) => !(plan.dossier?.status in ['in voorbereiding', 'vastgesteld', 'niet in werking']))
+        .sort((a: any, b: any) => {
+          const x = a['planstatusInfo']['datum']
+          const y = b['planstatusInfo']['datum']
+          if (x < y) return 1
+          if (y < x) return -1
+          return 0
+        })
+      const selectedPlans = relevantPlans
+        .filter((p) => !p.isParapluplan)
+        .slice(0, 1)
+        .concat(relevantPlans.filter((p) => p.isParapluplan).slice(0, 1))
+
+      console.log(
+        'Selected plans:',
+        selectedPlans.map((p: any) => p.id),
+      )
+
+      for (const plan of selectedPlans) {
         await requestToQuads([naam, `maatvoeringen-${plan.id}`], {
           path: `/plannen/${plan.id}/maatvoeringen/_zoek`,
           body: { _geo: { intersects: footprint } },
